c354bf1ee632170905f6a9ccba98ef1166eca3de
Zubad <zubad.ali95@gmail.com>
2022-10-22 13:38:20 +0500

    using arrays

---
+++ arrowsPlusObjects/arrowFunc.js
@@ -0,0 +1,20 @@
+/*
+var sum = (a, b) => {
+    var c = a+b;
+    return c
+}
+
+
+var result = sum(12, 13);
+
+console.log(result);
+*/
+var sum = function(a, b){
+    var c = a+b;
+    return c
+}
+
+
+var result = sum(12, 13);
+
+console.log(result);
